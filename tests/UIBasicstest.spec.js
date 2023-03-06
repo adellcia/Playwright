6025641f9376896af67da393fefd5edf0af97cbf
Ada Pokorska <ada.pokorska@itmagination.com>
2023-03-06 12:58:15 +0100

    3

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -24,7 +24,7 @@ test('1', async ({browser})=>
 });
 
 
-test.only('2', async ({browser})=>{
+test('2', async ({browser})=>{
 
     const context = await browser.newContext()
     const page = await context.newPage()
@@ -42,35 +42,41 @@ await expect(newPage.url()).toBe('https://webdriveruniversity.com/Contact-Us/con
 expect(await newPage.innerText('body')).toContain('Error: all fields are required');
 })
 
-test('3', async ({page})=>{
-    await page.goto('https://webdriveruniversity.com/');
-    await page.locator('#contact-us').evaluate((element) => {
-        element.removeAttribute('target');
-        element.click()
-    })
-    await page.fill('[placeholder="First Name"]', firstName);
-    await page.fill('[placeholder="Last Name"]', lastName);
-    await page.fill('[placeholder="Email Address"]', 'brad.pitt.gmail.com');
-    await page.fill('[placeholder="Comments"]', Comments);
-    await page.locator('input[type="submit"]').click()
-    await expect(page.url()).toBe('https://webdriveruniversity.com/Contact-Us/contact_us.php');
-expect(await page.innerText('body')).toContain('Error: Invalid email address');
+test('3', async ({browser})=>{
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto('https://webdriveruniversity.com/')
+    const pageLink = page.locator('#contact-us')
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        pageLink.click()
+    ])
+    await newPage.getByPlaceholder("First Name").fill(firstName)
+    await newPage.getByPlaceholder("Last Name").fill(lastName)
+    await newPage.getByPlaceholder("Email Address").fill('brad.pitt.gmail.com');
+    await newPage.getByPlaceholder("Comments").fill(Comments)
+    await newPage.locator('input[type="submit"]').click()
+    await expect(newPage.url()).toBe('https://webdriveruniversity.com/Contact-Us/contact_us.php');
+expect(await newPage.innerText('body')).toContain('Error: Invalid email address');
 
 })
 
-test('4', async ({page})=>{
-    await page.goto('https://webdriveruniversity.com/');
-    await page.locator('#contact-us').evaluate((element) => {
-        element.removeAttribute('target');
-        element.click()
-    })
-    await page.fill('[placeholder="First Name"]', firstName);
-    await page.fill('[placeholder="Last Name"]', lastName);
-    await page.fill('[placeholder="Email Address"]', emailAddress);
-    await page.fill('[placeholder="Comments"]', Comments);
-    await page.locator('input[type="submit"]').click()
-    await expect(page.url()).toBe('https://webdriveruniversity.com/Contact-Us/contact-form-thank-you.html')
-   
+test.only('4', async ({browser})=>{
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto('https://webdriveruniversity.com/')
+    const pageLink = page.locator('#contact-us')
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        pageLink.click()
+    ])
+    await newPage.getByPlaceholder("First Name").fill(firstName)
+    await newPage.getByPlaceholder("Last Name").fill(lastName)
+    await newPage.getByPlaceholder("Email Address").fill(emailAddress);
+    await newPage.getByPlaceholder("Comments").fill(Comments)
+    await newPage.locator('input[type="submit"]').click()
+    await expect(newPage.url()).toBe('https://webdriveruniversity.com/Contact-Us/contact-form-thank-you.html')
+    expect(await newPage.innerText('body')).toContain('Thank You for your Message!')
 })
 test('6', async ({page})=>{
     await page.goto('https://webdriveruniversity.com/');
